af6af4f4f1be21f7ecfc664230300e68a8f62991
Evgeniy Polovniy <epolovniy@webinerds.com>
2015-12-27 20:07:16 +0200

    add

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,24 +1,32 @@
 $(document).ready(function () {
 
   getBtcPrices();
-
-  function getBtcPrices () {
-    var url = "https://api.bitcoinaverage.com/ticker/global/all";
-    var xhr = new XMLHttpRequest();
-
-    xhr.open('GET', url, true);
-
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState != 4) return;
-      if (xhr.status != 200) {
-        console.log(xhr.status + ': ' + xhr.statusText);
-      } else {
-        var data = JSON.parse(xhr.response);
-        console.log(data);
-      }
-    }
-
-    xhr.send(null);
-  }
+  randomBg(1, 20);
 
 });
+
+function getBtcPrices () {
+  var url = "https://api.bitcoinaverage.com/ticker/global/all";
+  var xhr = new XMLHttpRequest();
+
+  xhr.open('GET', url, true);
+
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState != 4) return;
+    if (xhr.status != 200) {
+      console.log(xhr.status + ': ' + xhr.statusText);
+    } else {
+      var data = JSON.parse(xhr.response);
+      console.log(data);
+    }
+  }
+
+  xhr.send(null);
+}
+
+function randomBg (min, max) {
+  var rand = min - 0.5 + Math.random() * (max - min + 1)
+  rand = Math.round(rand);
+  var bgClass = 'bg-' + rand;
+  $('.js-bg').addClass(bgClass);
+}
